e54e64171c68e977b25e921a7269c4dce951fadb
Erick Y. Cárdenas Luque <133929770+ErickYCL@users.noreply.github.com>
2023-11-12 20:00:37 -0700

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,24 @@
+const cacheName = 'task-list-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
